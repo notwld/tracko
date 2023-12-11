55270f30f3bbdc3109a0983f8aac32ec95522b79
notwld <mwfarrukh@gmail.com>
2023-12-11 19:51:13 +0500

    modified chat ui

--- mobile/config/firebase.js
+++ mobile/config/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import Constants from "expo-constants";
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBIfxZmvl2VFal9Sojrdt27dISBvnp_1iE",
-  authDomain: "tracko-3d24a.firebaseapp.com",
-  projectId: "tracko-3d24a",
-  storageBucket: "tracko-3d24a.appspot.com",
-  messagingSenderId: "367905168890",
-  appId: "1:367905168890:web:5ddfac31a3de906f6c5843",
-  measurementId: "G-WBBQ6SYKY0"
+  apiKey: "AIzaSyAYGWhlxKSngstW_mzEE6oscRmOBPn0ebE",
+  authDomain: "tracko-f297a.firebaseapp.com",
+  projectId: "tracko-f297a",
+  storageBucket: "tracko-f297a.appspot.com",
+  messagingSenderId: "644418391939",
+  appId: "1:644418391939:web:b5b6d03a0d61c97d7a30b6",
+  measurementId: "G-EGE4QQT5HG"
   //   @deprecated is deprecated Constants.manifest
 };
 // initialize firebase
--- mobile/screens/Chat.js
+++ mobile/screens/Chat.js
@@ -1,105 +1,134 @@
 import React, {
-    useState,
-    useEffect,
-    useLayoutEffect,
-    useCallback
-  } from 'react';
-  import { TouchableOpacity, Text } from 'react-native';
-  import { GiftedChat } from 'react-native-gifted-chat';
-  import {
-    collection,
-    addDoc,
-    orderBy,
-    query,
-    onSnapshot
-  } from 'firebase/firestore';
-  import { signOut } from 'firebase/auth';
-  import { auth, database } from '../config/firebase';
-  import { useNavigation } from '@react-navigation/native';
-  import { AntDesign } from '@expo/vector-icons';
-  import colors from '../colors';
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useCallback
+} from 'react';
+import { TouchableOpacity, Text,View,Image } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import {
+  collection,
+  addDoc,
+  orderBy,
+  query,
+  onSnapshot
+} from 'firebase/firestore';
+import { signOut } from 'firebase/auth';
+import { auth, database } from '../config/firebase';
+import { useNavigation } from '@react-navigation/native';
+import { AntDesign } from '@expo/vector-icons';
+import colors from '../colors';
 
 
-  export default function Chat() {
+export default function Chat() {
 
-    const [messages, setMessages] = useState([]);
-    const navigation = useNavigation();
+  const [messages, setMessages] = useState([]);
+  const navigation = useNavigation();
 
-  const onSignOut = () => {
-      signOut(auth).catch(error => console.log('Error logging out: ', error));
-    };
+const onSignOut = () => {
+    signOut(auth).catch(error => console.log('Error logging out: ', error));
+  };
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-          headerRight: () => (
-            <TouchableOpacity
-              style={{
-                marginRight: 10
-              }}
-              onPress={onSignOut}
-            >
-              <AntDesign name="logout" size={24} color={colors.gray} style={{marginRight: 10}}/>
-            </TouchableOpacity>
-          )
-        });
-      }, [navigation]);
+  useLayoutEffect(() => {
+      navigation.setOptions({
+        headerRight: () => (
+          <TouchableOpacity
+            style={{
+              marginRight: 10
+            }}
+            onPress={onSignOut}
+          >
+            <AntDesign name="logout" size={24} color={colors.gray} style={{marginRight: 10}}/>
+          </TouchableOpacity>
+        )
+      });
+    }, [navigation]);
 
-    useLayoutEffect(() => {
+  useLayoutEffect(() => {
 
-        const collectionRef = collection(database, 'chats');
-        const q = query(collectionRef, orderBy('createdAt', 'desc'));
+      const collectionRef = collection(database, 'chats');
+      const q = query(collectionRef, orderBy('createdAt', 'desc'));
 
-    const unsubscribe = onSnapshot(q, querySnapshot => {
-        console.log('querySnapshot unsusbscribe');
-          setMessages(
-            querySnapshot.docs.map(doc => ({
-              _id: doc.data()._id,
-              createdAt: doc.data().createdAt.toDate(),
-              text: doc.data().text,
-              user: doc.data().user
-            }))
-          );
-        });
-    return unsubscribe;
-      }, []);
-
-    const onSend = useCallback((messages = []) => {
-        setMessages(previousMessages =>
-          GiftedChat.append(previousMessages, messages)
+  const unsubscribe = onSnapshot(q, querySnapshot => {
+      console.log('querySnapshot unsusbscribe');
+        setMessages(
+          querySnapshot.docs.map(doc => ({
+            _id: doc.data()._id,
+            createdAt: doc.data().createdAt.toDate(),
+            text: doc.data().text,
+            user: doc.data().user
+          }))
         );
-        // setMessages([...messages, ...messages]);
-        const { _id, createdAt, text, user } = messages[0];    
-        addDoc(collection(database, 'chats'), {
-          _id,
-          createdAt,
-          text,
-          user
-        });
-      }, []);
+      });
+  return unsubscribe;
+    }, []);
 
-      return (
-        // <>
-        //   {messages.map(message => (
-        //     <Text key={message._id}>{message.text}</Text>
-        //   ))}
-        // </>
-        <GiftedChat
-          messages={messages}
-          showAvatarForEveryMessage={false}
-          showUserAvatar={false}
-          onSend={messages => onSend(messages)}
-          messagesContainerStyle={{
-            backgroundColor: '#fff'
-          }}
-          textInputStyle={{
-            backgroundColor: '#fff',
-            borderRadius: 20,
-          }}
-          user={{
-            _id: auth?.currentUser?.email,
-            avatar: 'https://i.pravatar.cc/300'
-          }}
-        />
+  const onSend = useCallback((messages = []) => {
+      setMessages(previousMessages =>
+        GiftedChat.append(previousMessages, messages)
       );
+      // setMessages([...messages, ...messages]);
+      const { _id, createdAt, text, user } = messages[0];    
+      addDoc(collection(database, 'chats'), {
+        _id,
+        createdAt,
+        text,
+        user
+      });
+    }, []);
+    const renderMessage = props => {
+      const { currentMessage } = props;
+    
+      return (
+        <View
+          style={{
+            borderRadius: 10,
+            display: 'flex',
+            alignItems: 'flex-start',
+            justifyContent: 'center',
+            padding: 5,
+            margin: 10,
+            alignSelf: currentMessage.user._id === auth?.currentUser?.email ? 'flex-start' : 'flex-end',
+            maxWidth: '80%',
+          }}
+        >
+          <View style={{ display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+            <Image
+              source={{ uri: currentMessage.user.avatar }}
+              style={{ width: 30, height: 30, borderRadius: 15, marginRight: 7 }}
+            />
+            <View>
+              
+                <View style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row' }}>
+                  <Text style={{ paddingRight: 10, fontSize: 16, fontWeight: 'bold' }}>{currentMessage.user._id.split('@')[0]}</Text>
+                  <Text style={{ fontSize: 9, color: 'grey' }}>{currentMessage.createdAt.toLocaleTimeString()}</Text>
+                </View>
+              
+              <Text style={{ fontSize: 13 }}>{currentMessage.text}</Text>
+            </View>
+          </View>
+        </View>
+      );
+    };
+    return (
+      <GiftedChat
+        messages={messages}
+        showAvatarForEveryMessage={false}
+        showUserAvatar={false}
+        onSend={messages => onSend(messages)}
+        messagesContainerStyle={{
+          backgroundColor: '#fff'
+        }}
+        textInputStyle={{
+          backgroundColor: '#fff',
+          borderRadius: 20,
+        }}
+        user={{
+          _id: auth?.currentUser?.email,
+          avatar: 'https://i.pravatar.cc/300'
+        }}
+        renderMessage={renderMessage}
+      />
+    );
 }
 
